b833bf8f79132228da6adf9dae89a3a7384c4d0f
PythonDevZa <43183508+PythonDevZa@users.noreply.github.com>
2020-02-05 13:08:53 +0200

    Update website_blocker.py

--- website_blocker.py
+++ website_blocker.py
@@ -6,11 +6,23 @@ hosts_path = "etc/hosts"
 redirect = "127.0.0.1"
 website_list = ["https://github.com/pushshift/api", "https://www.github.com/pushshift/api", "http://redditsearch.io/", "http://www.redditsearch.io/"]
 
-while True:
-    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
-        print("Working hours....")
+if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
+        print("You should be working!")
+        with open(hosts_path,'r+') as file:
+            content=file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+" "+ website+"\n")
     else:
-        print("You're off")
-    time.sleep(5)    
-    
+        with open(hosts_path,'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+        print("Your off!")
+    time.sleep(5)
         
